2caf9fa98ba39b2d95c9bdede3ec445bdedda9c4
Nagalakshmi123455 <158137893+Nagalakshmi123455@users.noreply.github.com>
2024-04-01 18:46:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
-provider "aws" {
-  region = "us-east-1"
-}
-resource "aws_instance" "example" {
+resource "aws_instance" "localname" {
     ami = "ami-0c101f26f147fa7fd"
     instance_type = "t2.micro"
+    key_name = "terraform"
+    tags = {
+        name ="Terraform instance"
+    }
   
 }
